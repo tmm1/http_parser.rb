cdb2d51eb571dde4fb874e3644c724048189050a
Brian Lopez <seniorlopez@gmail.com>
2010-10-21 00:24:37 -0700

    add substitute strnstr implementation for platforms without it

--- ext/ruby_http_parser/extconf.rb
+++ ext/ruby_http_parser/extconf.rb
@@ -4,5 +4,6 @@ http_parser_dir = File.expand_path('../vendor/http-parser', __FILE__)
 $CFLAGS << " -I#{http_parser_dir} "
 $CFLAGS << "-ggdb3 -O0 -Wall -Wextra" if ENV['DEBUG']
 
+have_func("strnstr")
 dir_config("ruby_http_parser")
 create_makefile("ruby_http_parser")
--- ext/ruby_http_parser/ruby_http_parser.c
+++ ext/ruby_http_parser/ruby_http_parser.c
@@ -2,6 +2,31 @@
 #include "ext_help.h"
 #include "http_parser.c"
 
+/*
+ * Find the first occurrence of find in s, where the search is limited to the
+ * first slen characters of s.
+ */
+#ifndef HAVE_STRNSTR
+static char *strnstr(const char *s, const char *find, size_t slen) {
+  char c, sc;
+  size_t len;
+
+  if ((c = *find++) != '\0') {
+    len = strlen(find);
+    do {
+      do {
+        if (slen-- < 1 || (sc = *s++) == '\0')
+          return (NULL);
+      } while (sc != c);
+      if (len > slen)
+        return (NULL);
+    } while (strncmp(s, find, len) != 0);
+    s--;
+  }
+  return ((char *)s);
+}
+#endif
+
 #define GET_WRAPPER(N, from)  ParserWrapper *N = (ParserWrapper *)(from)->data;
 #define HASH_CAT(h, k, ptr, len)                \
   do {                                          \
