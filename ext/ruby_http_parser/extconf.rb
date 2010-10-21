6128f0ca449a09d80c27892bc3bb26702bed89bc
Brian Lopez <seniorlopez@gmail.com>
2010-10-20 22:21:58 -0700

    add ability to generate extension with debug symbols if DEBUG=1

--- ext/ruby_http_parser/extconf.rb
+++ ext/ruby_http_parser/extconf.rb
@@ -2,6 +2,7 @@ require 'mkmf'
 
 http_parser_dir = File.expand_path('../vendor/http-parser', __FILE__)
 $CFLAGS << " -I#{http_parser_dir} "
+$CFLAGS << "-ggdb3 -O0 -Wall -Wextra" if ENV['DEBUG']
 
 dir_config("ruby_http_parser")
 create_makefile("ruby_http_parser")
